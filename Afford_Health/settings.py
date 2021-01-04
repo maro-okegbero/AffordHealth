07634ac2683e472f4f727e2966ddf75cd892f64f
Maro <marookegbero@gmail.com>
2021-01-04 21:57:53 +0100

    changes

--- Afford_Health/settings.py
+++ Afford_Health/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["3af6323340ea.ngrok.io", '127.0.0.1', '.herokuapp.com']
 
